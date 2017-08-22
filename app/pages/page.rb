25e65e0457a59c5ec90145753ca2192c2a29e828
Ricky Hurtado <ricky.hurtado@orahq.com>
2017-08-22 18:43:15 +1200

    Update the Page class

--- app/pages/page.rb
+++ app/pages/page.rb
@@ -1,7 +1,6 @@
 class Page
   attr_reader :data,     # Optional. Content from database
               :params,   # Optional. Pass the POST or URL parameters
-              :data,     # Content coming from database
               :title,    # Also used in HTML title tag
               :subtitle, # Also used in HTML meta description
               :id,       # HTML id attribute to be used for Vanilla JS reference
@@ -10,8 +9,8 @@ class Page
   def initialize datasource=Object, params={}
     @data = datasource
     @params = params
-    @title = options.fetch :title, options[:title]
-    @subtitle = options.fetch :subtitle, options[:subtitle]
+    @title = options.fetch :title, ''
+    @subtitle = options.fetch :subtitle, ''
     @id = options.fetch :id, ''
     @js_path = options.fetch :js_path, ''
   end
